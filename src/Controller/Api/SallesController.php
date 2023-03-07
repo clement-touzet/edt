bdd88f6e9d39511e2219fd50ca48879e6af6953d
Clément Touzet <ctouzet001@iutbayonne.univ-pau.fr>
2023-03-07 08:36:01 +0100

    css et affichage des détails de /cours

--- src/Controller/Api/SallesController.php
+++ src/Controller/Api/SallesController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/salles', name: 'salles_')]
+#[Route('/api/salles', name: 'salles_')]
 class SallesController extends AbstractController
 {
     #[Route('', name: 'list')]
